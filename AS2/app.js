02009bb87f92b8db24875ebe244d69a792c8dc6f
Rodrigo Valentim <rodrigo.do.tango@gmail.com>
2017-02-12 18:18:37 -0200

    AS2 Final

---
+++ AS2/app.js
@@ -0,0 +1,56 @@
+(function () {
+        'use strict';
+
+        angular.module( 'ShoppingListCheckOff', [] )
+                .controller( 'ToBuyList', BuyListController )
+                .controller( 'AlreadyBoughtList', BoughtListController )
+                .service( 'ShoppingListCheckOffService', ShoppingListCheckOffService );
+
+                BuyListController.$inject = ['ShoppingListCheckOffService'];
+                function BuyListController( ShoppingListCheckOffService ) {
+                        var buyList = this;
+
+                        buyList.items = ShoppingListCheckOffService.getBuyItems();
+
+                        buyList.buy = function( index, name, quantity ) {
+                                ShoppingListCheckOffService.buyItem( index, name, quantity );
+                        }
+                }
+
+                BoughtListController.$inject = ['ShoppingListCheckOffService'];
+                function BoughtListController( ShoppingListCheckOffService ) {
+                        var boughtList = this;
+
+                        boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+                }
+
+                function ShoppingListCheckOffService() {
+                        var service = this;
+
+                        var buyItems = [
+                                { name:"Cookies", quantity:10 },
+                                { name:"Milk",    quantity:1 },
+                                { name:"Donuts",  quantity:20 }
+                        ];
+
+                        var boughtItems = [];
+
+                        service.buyItem = function( itemIdex, itemName, quantity ) {
+                                var item = {
+                                        name: itemName,
+                                        quantity: quantity
+                                };
+
+                                boughtItems.push( item );
+                                buyItems.splice( itemIdex, 1 );
+                        };
+
+                        service.getBuyItems = function () {
+                                return buyItems;
+                        };
+
+                        service.getBoughtItems = function () {
+                                return boughtItems;
+                        };
+                }
+})();
